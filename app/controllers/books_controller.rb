c747314b7af71cf8b3a4aad86a39e6bdf209dd46
KarzLightyear <k.k.yamato@gmail.com>
2021-07-03 06:08:08 +0000

    [finish]応用課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = current_user
+    @book_new = Book.new
   end
 
   def index
@@ -25,6 +26,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
   end
 
 
